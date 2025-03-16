f203e2a6697077280cd32f2d6e135ead82f04605
Mas-arief <autama715@gmail.com>
2025-03-16 16:45:29 +0700

    Arieffff

---
+++ app/Http/Controllers/ListBarangController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListBarangController extends Controller
+
+    //class ListBarangController extends Controller
+{
+    public function tampilkan($id, $nama) {
+        return view('list_barang', ['id' => $id, 'nama' => $nama]);
+    }
+}
+
